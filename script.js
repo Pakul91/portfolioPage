2675994b08d74f8102247c41a317a17254fca9ad
Tomasz <pakulniewicz.t@gmail.com>
2021-07-24 12:35:39 +0200

    issue 2

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ const projectListItem = document.querySelectorAll(".project-list-item");
 const closeBoxBtn = document.querySelectorAll(".close-box");
 const overlay = document.querySelector(".overlay");
 
+// Sotore data from the clicked project
 let clickedProject = "";
 
-const closeBox = function () {
-  overlay.classList.add("hidden");
-  projectBox.forEach((box) => box.classList.add("hidden"));
-};
-
+//Display clicked project-box and overlay
 projectListItem.forEach((box) =>
   box.addEventListener("click", function (e) {
     clickedProject = e.target.dataset.project;
     document.getElementById(clickedProject).classList.remove("hidden");
     overlay.classList.remove("hidden");
+    window.scrollTo(0, 0);
   })
 );
 
+// Close box and overlay when clicked on X
+const closeBox = function () {
+  overlay.classList.add("hidden");
+  projectBox.forEach((box) => box.classList.add("hidden"));
+};
+
+// Close box and overlay when Esc is pressed
 closeBoxBtn.forEach((btn) => btn.addEventListener("click", closeBox));
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") closeBox();
