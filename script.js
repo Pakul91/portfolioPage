0d3eac5131a43ba4e44a9ef4a0156e0de059fbdc
Tomasz <pakulniewicz.t@gmail.com>
2021-10-29 18:01:35 +0100

    adding certificate tooltip

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 "use strict";
 
-const projectBox = document.querySelectorAll(".project-box");
-const projectListItem = document.querySelectorAll(".project-list-item");
+const modalBox = document.querySelectorAll(".modal-box");
+const modalBoxAnchor = document.querySelectorAll(".modal-box-anchor");
 const closeBoxBtn = document.querySelectorAll(".close-box");
 const overlay = document.querySelector(".overlay");
 
 // Sotore data from the clicked project
 let clickedProject = "";
 
-//Display clicked project-box and overlay
-projectListItem.forEach((box) =>
+//Display clicked modal-box and overlay
+modalBoxAnchor.forEach((box) =>
   box.addEventListener("click", function (e) {
     clickedProject = e.target.dataset.project;
     document.getElementById(clickedProject).classList.remove("hidden");
     overlay.classList.remove("hidden");
-    window.scrollTo(0, 0);
+    history.pushState({ clickedProject }, ``, `./selected=${clickedProject}`);
+    // window.scrollTo(0, 0);
   })
 );
 
 // Close box and overlay when clicked on X
 const closeBox = function () {
   overlay.classList.add("hidden");
-  projectBox.forEach((box) => box.classList.add("hidden"));
+  modalBox.forEach((box) => box.classList.add("hidden"));
+  history.pushState({}, ``, `./`);
 };
 
 // Close box and overlay when Esc is pressed
 closeBoxBtn.forEach((btn) => btn.addEventListener("click", closeBox));
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") closeBox();
+});
+
+window.addEventListener("popstate", (e) => {
+  const path = window.location.pathname;
+
+  if (path.startsWith("/selected")) {
+    history.pushState({}, ``, `./`);
+  }
+  closeBox();
 });
